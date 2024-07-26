5ffba26c10ecb0ad0630ffe4c05c839472bfc16f
hedaetul <hedaetulislam143@gmail.com>
2024-07-26 11:52:10 +0600

    build initial design

--- components/Home.tsx
+++ components/Home.tsx
@@ -7,14 +7,14 @@ const Home = () => {
     <Box component="section" className="grid grid-cols-2">
       <Box component="div">
         <Typography className="text-[60px] font-[950]">
-          Hi, I'm{" "}
+          Hi, I`m{" "}
           <span className="inline text-[60px] font-[950] text-[#A5A6FF]">
             Hedaetul!
           </span>
           Creative Web Developer in Bangladesh
         </Typography>
         <Typography className="text-[18px] font-extralight">
-          I'm a Bangladesh based web developer
+          I`m a Bangladesh based web developer
         </Typography>
         <Box component="div" className="mt-9 flex gap-4">
           <Button
